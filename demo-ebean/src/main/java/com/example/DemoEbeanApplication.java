747abd8c09a8329b1d97a0bf22713547c1d7ae62
yuichiro umezawa <garbagetown@gmail.com>
2016-07-13 10:22:39 +0900

    rename the bootstrap class name

--- demo-ebean/src/main/java/com/example/DemoEbeanApplication.java
+++ demo-ebean/src/main/java/com/example/DemoEbeanApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoEbeanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoEbeanApplication.class, args);
 	}
 }
